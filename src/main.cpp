00a1da929ebdb53df34f8fa45723bc80de7da80c
André Michelle <andre.michelle@gmail.com>
2022-09-15 18:37:10 +0200

    moves pixel with joystick

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/10, /* dc=*/8,
                                            /* reset=*/9);
 int frame = 0;
 
+int xy[] = {32, 32};
+
 void setup() {
     led.setup();
     joystick.setup();
@@ -31,27 +33,33 @@ void loop() {
     u8g2.setCursor(u8g2.getStrWidth("FPS") + 3, 5);
     u8g2.println(fps());
 
+    u8g2.drawPixel(xy[0], xy[1]);
+
     if (joystick.get(Joystick::CENTER)) {
         u8g2.drawFilledEllipse(104, 32, 5, 5);
     } else {
         u8g2.drawEllipse(104, 32, 5, 5);
     }
     if (joystick.get(Joystick::UP)) {
+        if (xy[1] > 0) xy[1]--;
         u8g2.drawFilledEllipse(104, 20, 5, 5);
     } else {
         u8g2.drawEllipse(104, 20, 5, 5);
     }
     if (joystick.get(Joystick::LEFT)) {
+        if (xy[0] > 0) xy[0]--;
         u8g2.drawFilledEllipse(92, 32, 5, 5);
     } else {
         u8g2.drawEllipse(92, 32, 5, 5);
     }
     if (joystick.get(Joystick::DOWN)) {
+        if (xy[1] < u8g2.getHeight() - 1) xy[1]++;
         u8g2.drawFilledEllipse(104, 44, 5, 5);
     } else {
         u8g2.drawEllipse(104, 44, 5, 5);
     }
     if (joystick.get(Joystick::RIGHT)) {
+        if (xy[0] < u8g2.getWidth() - 1) xy[0]++;
         u8g2.drawFilledEllipse(116, 32, 5, 5);
     } else {
         u8g2.drawEllipse(116, 32, 5, 5);
